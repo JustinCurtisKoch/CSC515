77fbcc346880089cd3a177cd2e0c38c4afe95440
JustinCurtisKoch <justincurtiskoch@gmail.com>
2025-09-15 23:22:17 -0400

    Mod1-v2

--- CSC515-Mod1-PM.py
+++ CSC515-Mod1-PM.py
@@ -1,15 +1,19 @@
 # CSC515 Portfolio Milestone Module 1
 # Introduction to OpenCV (Open-Source Computer Vision Library)
 
-import numpy as np
+# Import the OpenCV library
 import cv2 as cv
 
 # Load an image from file
 image = cv.imread('Mod1_Numbers.jpg')
+
+# Display the image in a window
 cv.namedWindow('Image', cv.WINDOW_NORMAL)
 cv.imshow('Image', image)
+
+# Wait for a key press and close the window
 cv.waitKey(0)
 cv.destroyAllWindows()
-cv.imwrite('Numbers_mod1.png', image)
 
-#"C:\Users\justi\CSC515\Mod1_Numbers.jpg.jpg"
+# Save the image to a new file
+cv.imwrite('Numbers_Mod1.png', image)
